bf42a35d55c3bbc00ebf42196e9e007a808c9def
Kate Culhane <kathryn.culhane@gmail.com>
2021-07-12 11:56:54 -0700

    Copying old files over

---
+++ R/rmdoctor.R
@@ -0,0 +1,22 @@
+library(rmarkdown)
+library(htmltools)
+
+# Features of the docket template
+docket <- function(toc = TRUE, code_folding = "hide") {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "analysis",
+                     "resources", "style.css", package = "katereR")
+
+  # call the base html_document function
+  rmarkdown::html_document(theme = "flatly",
+                           css = css,
+                           toc = toc,
+                           toc_float = TRUE,
+                           toc_depth = 2,
+                           number_sections = FALSE,
+                           df_print = "paged",
+                           code_folding = code_folding,
+                           # includes = rmarkdown::includes(after_body = footer)
+  )
+}
