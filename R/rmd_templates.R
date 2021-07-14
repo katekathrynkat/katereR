c95d8ee4a929acd237d999e84cab84beb546b53c
Kate Culhane <kathryn.culhane@gmail.com>
2021-07-13 20:50:04 -0700

    update templates

--- R/rmd_templates.R
+++ R/rmd_templates.R
@@ -2,7 +2,7 @@ library(rmarkdown)
 library(htmltools)
 
 # Analysis template
-t_analysis <- function(toc = TRUE, code_folding = "hide") {
+t_analysis <- function(toc = TRUE, code_folding = "hide", df_print = "paged") {
 
   # get the locations of resource files located within the package
   css <- system.file("rmarkdown", "templates", "analysis",
@@ -10,88 +10,73 @@ t_analysis <- function(toc = TRUE, code_folding = "hide") {
 
   # call the base html_document function
   rmarkdown::html_document(theme = "flatly",
+                           highlight = "pygments",
                            css = css,
                            toc = toc,
                            toc_float = TRUE,
-                           toc_depth = 2,
+                           toc_depth = 3,
                            number_sections = FALSE,
-                           df_print = "paged",
+                           df_print = df_print,
                            code_folding = code_folding,
   )
 }
 
-# Data wrangling template
-# t_data_wrangling <- function(toc = TRUE, code_folding = "hide") {
-#   
-#   # get the locations of resource files located within the package
-#   css <- system.file("rmarkdown", "templates", "data_wrangling",
-#                      "resources", "style.css", package = "katereR")
-#   
-#   # call the base html_document function
-#   rmarkdown::html_document(theme = "flatly",
-#                            css = css,
-#                            toc = toc,
-#                            toc_float = TRUE,
-#                            toc_depth = 2,
-#                            number_sections = FALSE,
-#                            df_print = "paged",
-#                            code_folding = code_folding,
-#   )
-# }
-
 # Index template
-# t_index <- function(toc = TRUE, code_folding = "hide") {
-#   
-#   # get the locations of resource files located within the package
-#   css <- system.file("rmarkdown", "templates", "index",
-#                      "resources", "style.css", package = "katereR")
-#   
-#   # call the base html_document function
-#   rmarkdown::html_document(theme = "flatly",
-#                            css = css,
-#                            toc = toc,
-#                            toc_float = TRUE,
-#                            toc_depth = 2,
-#                            number_sections = FALSE,
-#                            df_print = "paged",
-#                            code_folding = code_folding,
-#   )
-# }
+t_index <- function(toc = FALSE, code_folding = "none", df_print = "paged") {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "index",
+                     "resources", "style.css", package = "katereR")
+
+  # call the base html_document function
+  rmarkdown::html_document(theme = "flatly",
+                           highlight = "pygments",
+                           css = css,
+                           toc = toc,
+                           toc_float = TRUE,
+                           toc_depth = 3,
+                           number_sections = FALSE,
+                           df_print = df_print,
+                           code_folding = code_folding,
+  )
+}
 
 # Metadata template
-# t_metadata <- function(toc = TRUE, code_folding = "hide") {
-#   
-#   # get the locations of resource files located within the package
-#   css <- system.file("rmarkdown", "templates", "metadata",
-#                      "resources", "style.css", package = "katereR")
-#   
-#   # call the base html_document function
-#   rmarkdown::html_document(theme = "flatly",
-#                            css = css,
-#                            toc = toc,
-#                            toc_float = TRUE,
-#                            toc_depth = 2,
-#                            number_sections = FALSE,
-#                            df_print = "paged",
-#                            code_folding = code_folding,
-#   )
-# }
+t_metadata <- function(toc = TRUE, code_folding = "hide", df_print = "paged") {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "metadata",
+                     "resources", "style.css", package = "katereR")
+
+  # call the base html_document function
+  rmarkdown::html_document(theme = "flatly",
+                           highlight = "pygments",
+                           css = css,
+                           toc = toc,
+                           toc_float = TRUE,
+                           toc_depth = 3,
+                           number_sections = FALSE,
+                           df_print = df_print,
+                           code_folding = code_folding,
+  )
+}
 
 # Simple template
-# t_simple <- function(toc = TRUE, code_folding = "hide") {
-#   
-#   # get the locations of resource files located within the package
-#   css <- system.file("rmarkdown", "templates", "simple",
-#                      "resources", "style.css", package = "katereR")
-#   
-#   # call the base html_document function
-#   rmarkdown::html_document(theme = "flatly",
-#                            css = css,
-#                            toc = toc,
-#                            toc_float = TRUE,
-#                            toc_depth = 2,
-#                            number_sections = FALSE,
-#                            df_print = "paged",
-#                            code_folding = code_folding,
-#   )
-# }
+t_simple <- function(toc = FALSE, code_folding = "none", df_print = "paged") {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "simple",
+                     "resources", "style.css", package = "katereR")
+
+  # call the base html_document function
+  rmarkdown::html_document(theme = "flatly",
+                           highlight = "pygments",
+                           css = css,
+                           toc = toc,
+                           toc_float = TRUE,
+                           toc_depth = 3,
+                           number_sections = FALSE,
+                           df_print = df_print,
+                           code_folding = code_folding,
+  )
+}
