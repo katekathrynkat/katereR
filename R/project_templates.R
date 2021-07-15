a76fdde506cf7dd47517f8c389745c490f7c50ad
Kate Culhane <kathryn.culhane@gmail.com>
2021-07-14 21:27:45 -0700

    update site

--- R/project_templates.R
+++ R/project_templates.R
@@ -26,7 +26,7 @@ eco_analysis <- function(path, ...) {
     "",
     "Description goes here",
     "",
-    paste0("View analyses at https://katekathrynkat.github.io/", path),
+    paste0("View analyses at https://katekathrynkat.github.io/", path, "/"),
     "",
     "## Directory structure",
     "",
@@ -36,14 +36,15 @@ eco_analysis <- function(path, ...) {
     "        \u251C\u2500\u2500 field            # Field/lab measurements",
     "        \u251C\u2500\u2500 literature       # Data compiled from the literature",
     "        \u2514\u2500\u2500 spatial          # Maps and other spatial files",
-    "    \u251C\u2500\u2500 docs             # Documents summarizing analyses (.html compiled from .Rmd)",
+    "    \u251C\u2500\u2500 docs             # Website HTML files (rendered from R_markdowns)",
     "    \u251C\u2500\u2500 output           # Figures (.png) and data (.csv) generated during analyses",
     "    \u251C\u2500\u2500 R_markdowns      # Code for analyses (.Rmd)",
     "    \u251C\u2500\u2500 R_scripts        # Scripts containing source code, functions, etc. (.R)",
     "        \u2514\u2500\u2500 archive          # Unorganized snippets of ill-fated code (view at your own risk)",
     "    \u251C\u2500\u2500 renv             # Package management with renv",
+    
     "    \u2514\u2500\u2500 README.md        # You are here",
-    "",
+    "    \u251C\u2500\u2500 docs             # Standalone HTML reports (rendered from R_markdowns)",
     "-----",
     "",
     "**Contact:** Kate Culhane, kathrynculhane@ucsb.edu",
@@ -55,22 +56,24 @@ eco_analysis <- function(path, ...) {
     ##### CREATE INITIAL R SCRIPT #####
     
     temp_script <- paste(
-      "# Temporary R script to help with project initialization",
-      "# DELETE AFTER USE",
-      "",
-      "# Create license",
-      "usethis::use_mit_license('Kathryn Culhane')",
-      "",
-      "# Initialize renv",
-      "renv::init()",
-      "",
-      "# Start git repo",
-      "usethis::use_git(message = 'initial commit')",
-      "usethis::use_github(protocol = 'https')",
-      "",
-      "# Install necessary packages to renv environment",
-      "install.packages('devtools')",
-      "devtools::install_github('katekathrynkat/katereR')",
+      '# Temporary R script to help with project initialization',
+      '# DELETE AFTER USE',
+      '',
+      '# Initialize renv',
+      'renv::init()',
+      '',
+      '# Install packages to renv environment',
+      'install.packages("usethis")',
+      'install.packages("devtools")',
+      'devtools::install_github("katekathrynkat/katereR")',
+      'install.packages("tidyverse")',
+      '',
+      '# Create license',
+      'usethis::use_mit_license("Kathryn Culhane")',
+      '',
+      '# Start git repo',
+      'usethis::use_git(message = "initial commit")',
+      'usethis::use_github(protocol = "https")',
       sep = '\n'
     )
     writeLines(temp_script, "temporary_script.R")
@@ -85,6 +88,7 @@ eco_analysis <- function(path, ...) {
   dir.create("R_markdowns", recursive = TRUE, showWarnings = FALSE)
   dir.create("R_scripts", recursive = TRUE, showWarnings = FALSE)
   dir.create("R_scripts/archive", recursive = TRUE, showWarnings = FALSE)
+  dir.create("reports", recursive = TRUE, showWarnings = FALSE)
   
   # create data folders
   if (dots[["data_external"]]) {
@@ -100,27 +104,38 @@ eco_analysis <- function(path, ...) {
     dir.create("data_raw/spatial", recursive = TRUE, showWarnings = FALSE)
   }
   
-  # create create_docs.R
+  # create create_reports.R
   create_docs <- paste(
-    "###############################",
-    "#####     Create docs     #####",
-    "###############################",
-    "",
-    "# Compile .Rmd documents as HTML reports",
-    "# Save in folder 'docs'",
-    "",
-    "# Index",
-    "rmarkdown::render('R_markdowns/index.Rmd', output_file = '../docs/index.html')",
-    "",
-    "# 01 Metadata",
-    "rmarkdown::render('R_markdowns/01_metadata.Rmd', output_file = '../docs/01_metadata.html')",
-    "",
-    "# 02 Data wrangling",
-    "rmarkdown::render('R_markdowns/02_data_wrangling.Rmd', output_file = '../docs/02_data_wrangling.html')",
-    "",
-    "# 03 First analysis",
-    "rmarkdown::render('R_markdowns/03_first_analysis.Rmd', output_file = '../docs/03_first_analysis.html')",
-    sep = "\n"
+    '##################################',
+    '#####     Create reports     #####',
+    '##################################',
+    '',
+    '# Compile .Rmd documents as a webpage and/or HTML reports',
+    '',
+    '# Packages',
+    'library(rmarkdown)',
+    'library(katereR)',
+    '',
+    '##### Create docs for website #####',
+    '# Render to /docs',
+    '',
+    'rmarkdown::render_site("example")',
+    '',
+    '##### Standalone HTML docs #####',
+    '# Render to /reports',
+    '',
+    '# Index',
+    'katereR::render_report("R_markdowns/index.Rmd",',
+    '                       output_file = "../reports/index.html")',
+    '',
+    '# 01 Metadata',
+    'katereR::render_report("R_markdowns/01_metadata.html",',
+    '                       output_file = "../reports/01_metadata.html")',
+    '',
+    '# 02 Data wrangling',
+    'katereR::render_report("R_markdowns/02_data_wrangling.html",',
+    '                       output_file = "../reports/02_data_wrangling.html")',
+    sep = '\n'
   )
   writeLines(create_docs, "R_scripts/create_docs.R")
 
@@ -131,31 +146,30 @@ eco_analysis <- function(path, ...) {
     
     # create site YAML
     site_yml <- paste(
-      paste0('name: "', path, '"'),
+      paste0('name: "', dots[["project_name"]], '"'),
+      'output_dir: "../docs"',
       'navbar:',
       paste0('  title: "', dots[["project_name"]], '"'),
       '  left:',
-      '    - text: "Home"',
-      '      icon: fa-home',
-      '      href: index.html',
-      '    - text: "Data"',
-      '      icon: table',
-      '      menu:',
-      '        - text: "Metadata"',
-      '          href: "01_metadata.html"',
-      '        - text: "Data wrangling"',
-      '          href: "02_data_wrangling.html"',
-      '    - text: "Analyses"',
-      '      icon: chart-line',
-      '      menu:',
-      '        - text: "First analysis"',
-      '          href: "03_first_analysis.html"',
+      '  - text: "Project home"',
+      '    href: index.html',
+      '    icon: fa-home',
+      '  - text: "Data"',
+      '    href: "01_metadata_html"',
+      '    icon: fa-table',
+      '  - text: "Analyses"',
+      '    menu:',
+      '    - text: "Data wrangling"',
+      '      href: "02_data_wrangling.html"',
+      '    - text: "First analysis"',
+      '      href: "03_first_analysis.html"',
+      '    icon: fa-chart-line',
       '  right:',
-      '    - icon: fa-github',
-      paste0('    - href: https://github.com/katekathrynkat/', path),
+      paste0('  - href: https://github.com/katekathrynkat/', path, '/'),
+      '    icon: fa-github',
       sep = "\n"
     )
-    writeLines(site_yml, "docs/_site.yml")
+    writeLines(site_yml, "R_markdowns/_site.yml")
   }
  
 }
