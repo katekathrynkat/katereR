e9b57e4da0ed5e6948bcf1757642d98707eb6561
Kate Culhane <kathryn.culhane@gmail.com>
2021-07-27 17:38:02 -0700

    update templates

--- R/rmd_templates.R
+++ R/rmd_templates.R
@@ -66,6 +66,26 @@ t_metadata <- function(toc = TRUE, code_folding = "hide", df_print = "paged") {
   )
 }
 
+# Notes template
+t_notes <- function(toc = FALSE, code_folding = "none", df_print = "paged") {
+  
+  # get the locations of CSS file located within the package
+  css <- system.file("rmarkdown", "templates",
+                     "CSS", "style_base.css", package = "katereR")
+  
+  # call the base html_document function
+  rmarkdown::html_document(theme = "flatly",
+                           highlight = "pygments",
+                           css = css,
+                           toc = toc,
+                           toc_float = TRUE,
+                           toc_depth = 3,
+                           number_sections = FALSE,
+                           df_print = df_print,
+                           code_folding = code_folding
+  )
+}
+
 # Simple template
 t_simple <- function(toc = FALSE, code_folding = "none", df_print = "paged") {
 
